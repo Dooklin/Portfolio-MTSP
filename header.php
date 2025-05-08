1dada7f696886ddaec0d0a3d717192c43b95e39b
Dooklin <elias.sch2018@outlook.de>
2025-05-08 12:26:43 +0200

    header centered

--- header.php
+++ header.php
@@ -1,5 +1,11 @@
 <header>
-    <div>Welcome to THE Portfolio Site</div>
-
-    <a href="logout.php">Abmelden</a>
+    <div id="logo-section">
+        <div id="logo">LOGO</div>
+    </div>
+    <div id="heading-section">
+        <h1 id="heading">Welcome to THE Portfolio Site</h1>
+    </div>
+    <div id="logout-section">
+        <a href="logout.php">Abmelden</a>
+    </div>
 </header>
--- index.php
+++ index.php
@@ -12,12 +12,19 @@ if(!isset($_SESSION['name'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Elias Schinkinger">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>Portfolio</title>
 </head>
 <body>
     <?php include "header.php" ?>
+    <div id="body-background">
+        <div id="other-background">
 
+        </div>
+    </div>
     
 </body>
 </html>
