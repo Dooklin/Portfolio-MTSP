77dbeafa9d4a88e06d3b77b6f93bf0078ecb3216
Dooklin <elias.sch2018@outlook.de>
2025-05-08 19:25:27 +0200

    Design login changes 2/3

--- login.php
+++ login.php
@@ -1,31 +1,29 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.typekit.net/dfj2dpe.css">
     <link rel="stylesheet" href="style.css">
     <title>Anmelden</title>
 </head>
 <body>
-    <div id="tst">
-        <div id="background">
-            <div id="login-container">
-                <div id="login-wrapper">
-                    <h1>Anmelden</h1>
-                    <form action="loginHandle.php" method="post" id="login-form">
-                        <input type="text" placeholder="Name" name="name" id="name-input">
-                        <input type="password" placeholder="Password" name="password" id="password-input">
-
-                        <input type="submit" value="Anmelden" name="submit" id="submit-login">
-                    </form>
-                </div>
+    <div id="background">
+        <div id="login-container">
+            <div id="login-wrapper">
+                <h1>Anmelden</h1>
+                <form action="loginHandle.php" method="post" id="login-form">
+                    <input type="text" placeholder="Name" name="name" id="name-input" class="login-input">
+                    <input type="password" placeholder="Password" name="password" id="password-input" class="login-input">
+                    
+                    <input type="submit" value="Anmelden" name="submit" id="submit-login" class="login-input">
+                </form>
             </div>
         </div>
     </div>
+    <svg id="triangle-bg" style="position: fixed; z-index: -1; width: 100%; height: 100%; top: 0; left: 0;"></svg>
+
+    <script src="spawnTriangles.js"></script>
+
 </body>
 </html>
