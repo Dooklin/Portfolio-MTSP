4e129c7a0ddb4aa490c8b5c91178c007142a8e85
Dooklin <elias.sch2018@outlook.de>
2025-05-08 11:10:56 +0200

    login Css

--- login.php
+++ login.php
@@ -9,16 +9,18 @@
     <title>Anmelden</title>
 </head>
 <body>
-    <div id="background">
-        <div id="login-container">
-            <div id="login-wrapper">
-                <h1>Anmelden</h1>
-                    <form action="loginHandle.php" method="post">
-                    <input type="text" password="Name" name="name" id="name-input">
-                    <input type="password" placeholder="Password" name="password" id="password-input">
+    <div id="tst">
+        <div id="background">
+            <div id="login-container">
+                <div id="login-wrapper">
+                    <h1>Anmelden</h1>
+                    <form action="loginHandle.php" method="post" id="login-form">
+                        <input type="text" placeholder="Name" name="name" id="name-input">
+                        <input type="password" placeholder="Password" name="password" id="password-input">
 
-                    <input type="submit" value="Anmelden" name="submit">
-                </form>
+                        <input type="submit" value="Anmelden" name="submit" id="submit-login">
+                    </form>
+                </div>
             </div>
         </div>
     </div>
